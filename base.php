1b6babedf1e9dcdada13379d551193c629ea554a
Jimmy Smutek <jimmy@smutek.net>
2016-02-27 23:32:41 -0500

    Remove unnecessary class
    Remove unnecessary .row class from base.php

--- base.php
+++ base.php
@@ -19,7 +19,7 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/header');
     ?>
     <div class="wrap container" role="document">
-      <div class="content row">
+      <div class="content">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
